0256f329f4c499fb688b4011727618e8f4467bd2
leitongxue <leitongxue@gmail.com>
2014-07-18 11:11:22 +0800

    竞价显示

--- heh/app/scripts/controllers/creat.js
+++ heh/app/scripts/controllers/creat.js
@@ -19,12 +19,18 @@ angular.module('myYoProjectApp')
             $location.path('/list')
         }
 
-        $scope.middle = JSON.parse(localStorage.getItem('activities'))||[];
+        var gg = JSON.parse(localStorage.getItem('activities'))||[];
+
+
+            for (var i = 0; i < gg.length; i++)
+            {
+
+                $scope.middle = gg[i].messages
+            }
+
 
         $scope.log="true";
         $scope.hh=function(){$scope.log="false"}
-
-
         $scope.HH=function firm()
         {
             if (confirm("你确定提交吗？"))
