1a3d5e3beb50d3f7d3c492cf371da4d43d7657cf
leitongxue <leitongxue@gmail.com>
2014-07-25 15:26:54 +0800

    .

--- heh/app/scripts/controllers/jingjia.js
+++ heh/app/scripts/controllers/jingjia.js
@@ -17,32 +17,52 @@ angular.module('myYoProjectApp')
 
         var gg = JSON.parse(localStorage.getItem('activities')) || [];
 
-        $scope.move=function()
-        {
-        for (var i = 0; i < gg.length; i++)
-        {
-            if (gg[i].tureth == "true"){}
-            else
-            {
-                    var shu={"shus":"shu","messages":[],"color":"color"}
+        $scope.move = function () {
+            for (var i = 0; i < gg.length; i++) {
+                if (gg[i].tureth == "true") {
+                }
+                else
+                {
+                    //定义数组
+                    var shu = {"shus": "shu", "messages": [], "color": "false"}
                     var shus = JSON.parse(localStorage.getItem('shus')) || [];
                     shus.unshift(shu);
                     localStorage.setItem("shus", JSON.stringify(shus));
-                    $location.path('/Money_message')
-
                     var shus = JSON.parse(localStorage.getItem('shus')) || [];
-                    shus[0].shus="竞价"+shus.length
-                    localStorage.setItem("shus",JSON.stringify(shus))
+                    console.log(shus.length)
+
+                    //点击开始按钮，竞价活动+1
+                    var shus = JSON.parse(localStorage.getItem('shus')) || [];
+                    shus[0].shus = "竞价" + shus.length
+                    shus[0].color = "true"
+                    localStorage.setItem("shus", JSON.stringify(shus))
+                    $location.path('/Money_message')
                     break
+                }
+            }
+        }
+
+        function dian()
+        {
+            var shus = JSON.parse(localStorage.getItem('shus')) || [];
+            for(var k = 0; k < shus.length; k++)
+            {
+                if(shus[k].color == "true")
+                {
+                    $scope.dian=true;
+                    break;
+                }
+                else
+                {
+                    $scope.dian=false;
+                }
             }
 
+        }
+dian()
 
-//                if(){}
-        }
-        }
-        $scope.list2=JSON.parse(localStorage.getItem('shus'))||[];
-        $scope.back=function()
-        {
+        $scope.list2 = JSON.parse(localStorage.getItem('shus')) || [];
+        $scope.back = function () {
             $location.path('/creat')
         }
     })
