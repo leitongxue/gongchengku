b1094e281335602691d634a06cf993c96ffe7f93
leitongxue <leitongxue@gmail.com>
2014-07-29 16:02:21 +0800

    use _.find

--- heh/app/scripts/sms.js
+++ heh/app/scripts/sms.js
@@ -2,7 +2,7 @@
 //notify_message_received({"messages":[{"create_date":"Tue Jan 15 15:28:44 格林尼治标准时间+0800 2013","message":"jj308","phone":"18733171780"}]})
 var native_accessor = {
     send_sms: function (phone, message) {
-        native_access.send_sms({"receivers":[{"name":'name', "phone":phone}]}, {"message_content":message});
+//        native_access.send_sms({"receivers":[{"name":'name', "phone":phone}]}, {"message_content":message});
         console.log(phone, message);
     },
 
@@ -68,20 +68,24 @@ var native_accessor = {
                     {
                         if (activity[a].activity == oo)//找到当前报名活动
                         {
-                            for (var j = 0; j < activity[a].messages.length; j++)//遍历当前活动的报名信息
+                            var action = _.find(activity, function (act) {
+                                return act.activity == localStorage.ttt
+                            }).messages
+                            var even = _.find(action, function (act) {
+                                return act.phone == message.phone
+                            })
+                            if (even)//判断竞价人是否报名该活动
                             {
-                                if (activity[a].messages[j].phone == message.phone)//判断竞价人是否报名该活动
-                                {
-                                    shus[0].messages.unshift(message)
-                                    localStorage.setItem("shus", JSON.stringify(shus))
-                                    native_accessor.send_sms(json_message.messages[0].phone,"恭喜您，竞价成功!")
-                                    refresh_pages()
-                                    break
-                                }
-                                else {
-                                    native_accessor.send_sms(json_message.messages[0].phone,"未报名活动，不能参加竞价")
-                                    break
-                                }
+                                console.log(2)
+                                shus[0].messages.unshift(message)
+                                localStorage.setItem("shus", JSON.stringify(shus))
+                                native_accessor.send_sms(json_message.messages[0].phone, "恭喜您，竞价成功!")
+                                refresh_pages()
+//                              break
+                            }
+                            else {
+                                native_accessor.send_sms(json_message.messages[0].phone, "未报名活动，不能参加竞价1")
+                                break
                             }
                         }
                     }
@@ -91,7 +95,7 @@ var native_accessor = {
                     for (var i = 0; i < shus[0].messages.length; i++) {
                         if (message.phone == shus[0].messages[i].phone) {
 //                            native_accessor.send_sms(json_message.messages[0].phone, "报名已成功，请勿重复报名")
-                            native_accessor.send_sms(json_message.messages[0].phone,"竞价已成功，请勿重复竞价")
+                            native_accessor.send_sms(json_message.messages[0].phone, "竞价已成功，请勿重复竞价")
                             return;
                         }
                         else {
@@ -101,23 +105,23 @@ var native_accessor = {
 
                                 if (activity[a].activity == oo)//找到当前活动
                                 {
-                                    var action= _.find(activity,function(act){return act.activity==localStorage.ttt}).messages
+                                    var action = _.find(activity, function (act) {
+                                        return act.activity == localStorage.ttt
+                                    }).messages
 //                                    console.log(action)
-                                    var even= _.find(action,function(act){ return act.phone==message.phone})
-//                                    console.log(even)
-
-//                                    console.log(1)
-//                                    console.log(even)
+                                    var even = _.find(action, function (act) {
+                                        return act.phone == message.phone
+                                    })
                                     if (even)//判断竞价人是否,li8报名
                                     {
                                         shus[0].messages.unshift(message)
                                         localStorage.setItem("shus", JSON.stringify(shus))
-                                        native_accessor.send_sms(json_message.messages[0].phone,"恭喜您，竞价成功！")
+                                        native_accessor.send_sms(json_message.messages[0].phone, "恭喜您，竞价成功！")
                                         refresh_pages()
 //                                          break;
                                     }
                                     else {
-                                        native_accessor.send_sms(json_message.messages[0].phone,"未报名活动，不能参加竞价")
+                                        native_accessor.send_sms(json_message.messages[0].phone, "未报名活动，不能参加竞价")
                                         return
                                     }
 
